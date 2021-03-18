9fb4fe6b72992c7474bf7a05fd3cb8486b819cc8
taroj khan <tarujkhan@yahoo.com>
2021-03-18 22:55:14 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-require 'pry'
+#require 'pry'
 class SessionsController < ApplicationController
   def new
   end
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: params[:name])
 
-    return head(:forbidden) unless @user.authenticate(params[:user][:password])
-    binding.pry
+    return flash[:alert] = "Incorrect User" unless @user.authenticate(params[:password])
+    #binding.pry
     session[:user_id] = @user.id
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210317235515) do
+ActiveRecord::Schema.define(version: 20210318223006) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "password"
   end
 
 end
