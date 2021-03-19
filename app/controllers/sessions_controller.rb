3e4eac4dc5dfc32794c8102112ca1092a209923f
taroj khan <tarujkhan@yahoo.com>
2021-03-19 00:59:55 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,21 @@
-#require 'pry'
+require 'pry'
 class SessionsController < ApplicationController
   def new
   end
 
   def create
-    @user = User.find_by(name: params[:name])
-
-    return flash[:alert] = "Incorrect User" unless @user.authenticate(params[:password])
+    @user =  User.find_by(name: params[:user][:name]) # if it returns an object it will be true else nil
     #binding.pry
+    if @user && @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
+
+    redirect_to home_path #contoller: "welcome", action: "home"
+  else
+    redirect_to controller: "sessions", action: "new"
+    #binding.pry
+    #return flash[:alert] = "Incorrect User" unless @user.authenticate(params[:password])
+    #return flash[:alert] = "Incorrect User" unless params[:password] == @user.password
+  end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,23 @@
+require 'pry'
 class UsersController < ApplicationController
 
   def new
   end
 
   def create
+    if params["user"]["password"] == params["user"]["password_confirmation"]
+
     @user = User.create(user_params)
+    session[:user_id] = @user.id
+  #  binding.pry
+  else
+    redirect_to "/users/new"
+    end
   end
 
-end
+  private
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 
-private
-def user_params
-  params.require(:user).permit(:name, :password, :password_confirmation)
 end
